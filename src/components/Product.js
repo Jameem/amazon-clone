5ae0473791a12914c192efc9f79e151851f5ad9c
Jameem <jameemkurikkal.mp@gmail.com>
2020-07-29 09:24:12 +0530

    product component

---
+++ src/components/Product.js
@@ -0,0 +1,30 @@
+import React from "react"
+import Star from "@material-ui/icons/Star"
+import "./Product.css"
+
+function Product({ id, title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>{price}</strong>
+        </p>
+        <div className="product__rating">
+          {Array(rating)
+            .fill()
+            .map((_) => (
+              <Star className="" />
+            ))}
+        </div>
+        <img className="product__image" src={image} alt="" />
+        <center>
+          <button className="product__button">Add to Basket</button>
+        </center>
+      </div>
+    </div>
+  )
+}
+
+export default Product
