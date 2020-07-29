4fef1cf885d6143fbcbbe4a1efdb0145711ba613
Jameem <jameemkurikkal.mp@gmail.com>
2020-07-29 09:24:54 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+
 import "./App.css"
+import Header from "./components/Header"
+import Home from "./components/Home"
 
 function App() {
   return (
-    <div className="App">
-      <h1>hii</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path="/checkout">
+            <Header />
+            <h1>Checkout</h1>
+          </Route>
+
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
