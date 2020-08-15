6da894d960eab33e75bdd59e453432d7d4a1a6a1
Jameem <jameemkurikkal.mp@gmail.com>
2020-08-15 19:23:41 +0530

    added add to basket on product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,24 @@
 import React from "react"
 import Star from "@material-ui/icons/Star"
 import "./Product.css"
+import { useStateValue } from "../StateProvider"
 
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue()
+
+  const addToBasket = () => {
+    // add item to basket
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    })
+  }
   return (
     <div className="product">
       <div className="product__info">
@@ -21,7 +37,9 @@ function Product({ id, title, image, price, rating }) {
       </div>
       <img className="product__image" src={image} alt="" />
       <center>
-        <button className="product__button">Add to Basket</button>
+        <button className="product__button" onClick={addToBasket}>
+          Add to Basket
+        </button>
       </center>
     </div>
   )
