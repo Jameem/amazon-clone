4bdef6faca73f392afc573f3585c9a3f25eba16d
Jameem <jameemkurikkal.mp@gmail.com>
2020-08-15 22:17:28 +0530

    packages

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,9 @@
-export const initialState = { basket: [] }
+export const initialState = {
+  basket: [],
+}
+
+export const getBasketTotal = (basket) =>
+  basket?.reduce((amount, item) => item.price + amount, 0)
 
 const reducer = (state, action) => {
   switch (action.type) {
