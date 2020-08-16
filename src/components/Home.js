193b02d429304d8d4b1bd6183dac09007fc2e721
Jameem <jameemkurikkal.mp@gmail.com>
2020-08-16 09:00:17 +0530

    cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,14 +9,16 @@ function Home() {
       <img className="home__image" src="banner.jpg" />
       <div className="home__row">
         <Product
-          id="1212"
+          id="10001"
+          key="10001"
           title='ASUS ZenBook Pro Duo UX581 Intel Core i7 9th Gen 15.6" 4K UHD OLED Touchscreen Laptop'
           price={2999.99}
           rating={5}
           image="https://images-na.ssl-images-amazon.com/images/I/91VR2ZXL9LL._SY355_.jpg"
         />
         <Product
-          id="1212"
+          id="10002"
+          key="10002"
           title="Apple iPhone 11 Pro Max (256GB) - Space Grey"
           price={1759.99}
           rating={4}
@@ -25,21 +27,24 @@ function Home() {
       </div>
       <div className="home__row">
         <Product
-          id="1212"
+          id="10003"
+          key="10003"
           title="BUXAZ Activity Tracker"
           price={24.99}
           rating={4}
           image="https://images-na.ssl-images-amazon.com/images/I/61mCMm4EZ6L._SX679_.jpg"
         />
         <Product
-          id="1212"
+          id="10004"
+          key="10004"
           title="Mammon Women's Handbag With Sling Bag & Clutch (Set of 3) (3LR-bib-Green-Tie_Green)"
           price={18.99}
           rating={5}
           image="https://images-na.ssl-images-amazon.com/images/I/71QDWj8ThIL._UX575_.jpg"
         />
         <Product
-          id="1212"
+          id="10005"
+          key="10005"
           title="Alex Vando Mens Dress Shirts Regular Fit Long Sleeve Men Shirt"
           price={18.99}
           rating={5}
