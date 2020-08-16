6772b2c82bd4f7304e3631feb78404c506e4342a
Jameem <jameemkurikkal.mp@gmail.com>
2020-08-16 09:01:25 +0530

    handle login and logout with useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import React from "react"
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import React, { useEffect } from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 import "./App.css"
 import Header from "./components/Header"
 import Home from "./components/Home"
 import Checkout from "./components/Checkout"
+import Login from "./components/Login"
+import { useStateValue } from "./StateProvider"
+import { auth } from "./firebase"
 
 function App() {
+  const [{ user }, dispatch] = useStateValue()
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged((authUser) => {
+      if (authUser) {
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        })
+      } else {
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        })
+      }
+    })
+
+    return () => {
+      // Cleanup goes here
+      unsubscribe()
+    }
+  }, [])
+
   return (
     <Router>
       <div className="app">
@@ -17,7 +43,7 @@ function App() {
           </Route>
 
           <Route path="/login">
-            <h1>Login</h1>
+            <Login />
           </Route>
 
           <Route path="/">
